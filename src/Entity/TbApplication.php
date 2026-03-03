c5beae0a6da4e8b62c07fe3621efdf67d7d21433
herve nsumbu <hervebitshila@gmail.com>
2026-03-03 12:16:59 +0100

    mise en place de la base des donnees et entite

---
+++ src/Entity/TbApplication.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TbApplicationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TbApplicationRepository::class)]
+class TbApplication
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $key_id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $secret_key = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getKeyId(): ?int
+    {
+        return $this->key_id;
+    }
+
+    public function setKeyId(int $key_id): static
+    {
+        $this->key_id = $key_id;
+
+        return $this;
+    }
+
+    public function getSecretKey(): ?string
+    {
+        return $this->secret_key;
+    }
+
+    public function setSecretKey(string $secret_key): static
+    {
+        $this->secret_key = $secret_key;
+
+        return $this;
+    }
+}
---
+++ src/Repository/TbUserRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\TbUser;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<TbUser>
+ */
+class TbUserRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, TbUser::class);
+    }
+
+    //    /**
+    //     * @return TbUser[] Returns an array of TbUser objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('t.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?TbUser
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
